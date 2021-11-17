a2d0a7c04ada829eb22de76ce640e5fcfa94b72c
an aspirin <beol0127@gmail.com>
2021-11-17 13:45:15 +0000

    Update files

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <mainapp msg="e efsf dse"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import mainapp from './components/mainapp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    mainapp
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
---
+++ src/components/mainapp.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1> {{ msg }} </h1>
+</template>
+
+<script>
+export default {
+  name: 'mainapp',
+  props: {
+    msg: String
+  }
+}
+</script>
